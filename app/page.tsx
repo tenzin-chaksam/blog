8034433443c6a0f5128a218664c43ebafe5316fb
Tenzin <chaksamt@gmail.com>
2025-08-04 06:04:30 -0500

    ecma

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import Link from "next/link";
-
-const AboutContent = dynamic(() => import("./content/ecmascript-2025.mdx"), {
-  ssr: false,
-});
 
 export default function AboutPage() {
   return (
-    <p>
-      I will be posting here soon. Meanwhile check out my{" "}
-      <Link href="about">bio</Link>.
-    </p>
-    // <main className="prose dark:prose-invert mx-auto p-6">
-    //   <AboutContent />
-    // </main>
+    <main className="max-w-2xl font-mono m-auto mb-10 text-sm">
+      <li>
+        <Link href={"/2025/ecmascript-2025"} className="block">
+          <span
+            className={`flex transition-[background-color] hover:bg-gray-100 dark:hover:bg-[#242424] active:bg-gray-200 dark:active:bg-[#222] border-y border-gray-200 dark:border-[#313131]
+                ${false ? "border-t-0" : ""}
+                ${true ? "border-b-0" : ""}
+              `}
+          >
+            <span
+              className={`py-3 flex grow items-center ${!false ? "ml-14" : ""}`}
+            >
+              <span className="grow dark:text-gray-100">
+                What's new in ECMAScript 2025
+              </span>
+            </span>
+          </span>
+        </Link>
+      </li>
+    </main>
   );
 }
